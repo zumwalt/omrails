e9a097b91a092f5ab73bd2fe1b4756f8dfed5de0
Casey Zumwalt <casey@simplefocus.com>
2013-09-26 08:05:23 -0500

    Added image uploading via url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
   has_attached_file :image, styles: { medium: "320x240>" }
 
 
@@ -11,5 +11,8 @@ class Pin < ActiveRecord::Base
 
   belongs_to :user
 
-
+  def image_remote_url=(url_value)
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
+  end
 end
